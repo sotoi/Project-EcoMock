daa51fc76e585377f2ba83380f45124ac34818a4
Samantha Pham <samanthavpham@gmail.com>
2022-02-12 10:46:14 -0800

    updated App to show review breakdown

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import {useParams,} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {fetchProductId} from '../redux/store.js'
+<<<<<<< HEAD
 import {getStyles, getRelated} from './helpers/main_helpers.jsx'
 import Overview from './product_info/Overview.jsx'
 import Review from './reviews/Review.jsx';
 import Related from './related_items/Related.jsx'
+=======
+import ReviewBreakdown from './reviews/ReviewBreakdown.jsx';
+>>>>>>> 019928f (updated App to show review breakdown)
 
 function App() {
   const { id } = useParams();
@@ -22,10 +26,17 @@ function App() {
   return (
 
     <div>
+<<<<<<< HEAD
       <Overview styles={styles}/>
       <Related related={related} styles={styles} />
       {/* <Review/> */}
+=======
+      {/* hi we are loading */}
+      {JSON.stringify(product)}
+      <ReviewBreakdown />
+>>>>>>> 019928f (updated App to show review breakdown)
     </div>
   );
 }
+
 export default App;
