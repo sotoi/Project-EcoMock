b7db20211b8707a79776be580c9dc169cd4798a2
Samantha Pham <samanthavpham@gmail.com>
2022-02-12 10:47:27 -0800

    created ReviewBreakdown component

---
+++ client/src/components/reviews/ReviewBreakdown.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getReviewsMetadata } from '../helpers/main_helpers.jsx';
+
+const ReviewBreakdown = () => {
+  const product = useSelector((state) => state.product);
+  const [metadata, setMetadata] = useState({});
+
+  useEffect(() => {
+    let product_id = product.value.id;
+    if (product_id) {
+      getReviewsMetadata(product_id, setMetadata);
+    }
+  }, [product]);
+
+  return (
+    <div>{JSON.stringify(metadata)}</div>
+  );
+}
+
+export default ReviewBreakdown;
